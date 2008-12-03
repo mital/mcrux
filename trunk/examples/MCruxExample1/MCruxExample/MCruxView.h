b5ae20003e4f5ef12b007023567c666cac8eabd0
mital.d.vora <mital.d.vora@8fcffb50-ab33-11dd-9a14-c1645b63b0c6>
2008-12-03 01:27:16 +0000

    Adding example derived from WebKit WebLauncher Example.

---
+++ trunk/examples/MCruxExample1/MCruxExample/MCruxView.h
@@ -0,0 +1,27 @@
+#pragma once
+
+#include "WebKit.h"
+#include "MCruxWebHost.h"
+#include "AddressBar.h"
+
+
+class MCruxView
+{
+	HWND gViewWindow;
+	IWebView* gWebView;
+	MCruxWebHost* gWebHost;
+	AddressBar* addressBar;
+
+public:
+	MCruxView(HWND hMainWnd, AddressBar * _addressBar);
+
+	~MCruxView();
+
+	void resize(RECT rcClient);
+
+	void showWindow(int nCmdShow);
+
+	void loadURL(BSTR urlBStr);
+
+	BSTR getFrameURL(IWebView* webView);
+};
