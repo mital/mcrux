180b51ff11f95cdc594aa7e05e193141ddfe3639
mital.d.vora <mital.d.vora@8fcffb50-ab33-11dd-9a14-c1645b63b0c6>
2008-12-14 04:14:54 +0000

    JS to c++ calls. Refactoring the MCrux class.

--- examples/JS to C++ Calling/MCruxExample2/MCruxExample2/MCrux.cpp
+++ examples/JS to C++ Calling/MCruxExample2/MCruxExample2/MCrux.cpp
@@ -1,10 +1,33 @@
 #include "StdAfx.h"
 #include "MCrux.h"
 
-JSStaticFunction mcruxDefaultFunctions[] = { {"someFunction", MCrux::someFunctionCallback, 0}, {0, 0, 0} };
+#include <JavaScriptCore/JSContextRef.h>
+#include <JavaScriptCore/JSRetainPtr.h>
+#include <JavaScriptCore/JSStringRefCF.h>
+#include <JavaScriptCore/RetainPtr.h>
+
+
+JSStaticFunction MCrux::mcruxDefaultFunctions[] = { {"someFunction", MCrux::someFunctionCallback, 0}, {0, 0, 0} };
 
 MCrux::MCrux()
 {
+	MCruxNameSpace.version = 0;
+	MCruxNameSpace.attributes = 0;
+	MCruxNameSpace.className = "mcrux";
+	MCruxNameSpace.parentClass = 0;
+	MCruxNameSpace.staticValues = 0;
+	MCruxNameSpace.staticFunctions = mcruxDefaultFunctions;
+	MCruxNameSpace.initialize = initializeMCrux;
+	MCruxNameSpace.finalize = finalizeMCrux;
+	MCruxNameSpace.hasProperty = 0;
+	MCruxNameSpace.getProperty = 0;
+	MCruxNameSpace.setProperty = 0;
+	MCruxNameSpace.deleteProperty = 0;
+	MCruxNameSpace.getPropertyNames = 0;
+	MCruxNameSpace.callAsFunction = 0;
+	MCruxNameSpace.callAsConstructor = 0;
+	MCruxNameSpace.hasInstance = 0;
+	MCruxNameSpace.convertToType = 0;
 }
 
 MCrux::~MCrux()
@@ -21,7 +44,7 @@ JSObjectRef MCrux::createJSWrapper(JSContextRef context)
 JSValueRef MCrux::someFunctionCallback(JSContextRef ctx, JSObjectRef function, JSObjectRef thisObject, size_t argumentCount, const JSValueRef arguments[], JSValueRef *exception)
 {
 	::MessageBoxA(0, "someFunctionCallback called", "test", MB_OK);
-	return 0;
+	return (JSValueRef)JSStringCreateWithUTF8CString(":My Returned Value From Function:");
 }
 
 void MCrux::initializeMCrux(JSContextRef ctx, JSObjectRef object)
