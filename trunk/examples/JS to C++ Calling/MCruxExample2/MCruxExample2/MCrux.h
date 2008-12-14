5c79dfd4b52c3a7ef3877b2bf21ff9b6da672500
mital.d.vora <mital.d.vora@8fcffb50-ab33-11dd-9a14-c1645b63b0c6>
2008-12-14 04:14:54 +0000

    JS to c++ calls. Refactoring the MCrux class.

--- trunk/examples/JS to C++ Calling/MCruxExample2/MCruxExample2/MCrux.h
+++ trunk/examples/JS to C++ Calling/MCruxExample2/MCruxExample2/MCrux.h
@@ -3,42 +3,21 @@
 
 #include <JavaScriptCore/JSObjectRef.h>
 
-JSStaticFunction mcruxDefaultFunctions[];
-
 
 class MCrux
 {
+	static JSStaticFunction mcruxDefaultFunctions[2];
+
+	static JSValueRef someFunctionCallback(JSContextRef ctx, JSObjectRef function, JSObjectRef thisObject, size_t argumentCount, const JSValueRef arguments[], JSValueRef *exception);
+	static void initializeMCrux(JSContextRef ctx, JSObjectRef object);
+	static void finalizeMCrux(JSObjectRef object);
+
+	JSClassDefinition MCruxNameSpace;
+
 public:
 	MCrux();
 	~MCrux();
 
 	JSObjectRef createJSWrapper(JSContextRef context);
 
-	static JSValueRef someFunctionCallback(JSContextRef ctx, JSObjectRef function, JSObjectRef thisObject, size_t argumentCount, const JSValueRef arguments[], JSValueRef *exception);
-
-	static void initializeMCrux(JSContextRef ctx, JSObjectRef object);
-	static void finalizeMCrux(JSObjectRef object);
-
 };
-
-
-static JSClassDefinition MCruxNameSpace = {0,
-0,
-"mcrux",
-0,
-0,
-mcruxDefaultFunctions,
-MCrux::initializeMCrux, // function that will be called the first time this class is created
-MCrux::finalizeMCrux, // function that will be called to tear down this class
-0, 
-0,
-0,
-0,
-0, 
-0,
-0,
-0,
-0,
-};
-
-
