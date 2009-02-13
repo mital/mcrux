b8ae00e3c55bb8490d5ea1ebae802b31e1430cac
mital.d.vora <mital.d.vora@8fcffb50-ab33-11dd-9a14-c1645b63b0c6>
2009-02-13 01:22:09 +0000

    Adding callJavaScriptFunction() in MCruxJSObject class. now u can call a JavaScriptFunction passing its name to mcrux.callJavaScriptFunction("myfunc").

--- trunk/MCrux/MCrux/plugin/MCruxJSObject.cpp
+++ trunk/MCrux/MCrux/plugin/MCruxJSObject.cpp
@@ -1,21 +1,21 @@
 /**
- * copyright (C) 2008 Mital Vora. All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met:
- *
- * 1.  Redistributions of source code must retain the above copyright
- *     notice, this list of conditions and the following disclaimer.
- * 2.  Redistributions in binary form must reproduce the above copyright
- *     notice, this list of conditions and the following disclaimer in the
- *     documentation and/or other materials provided with the distribution.
- * 3.  Neither the name of MCrux nor the names of its contributors may be 
- *     used to endorse or promote products derived from this software 
- *     without specific prior written permission.
- *
- * @author: Mital Vora.
- **/
+* copyright (C) 2008 Mital Vora. All rights reserved.
+*
+* Redistribution and use in source and binary forms, with or without
+* modification, are permitted provided that the following conditions
+* are met:
+*
+* 1.  Redistributions of source code must retain the above copyright
+*     notice, this list of conditions and the following disclaimer.
+* 2.  Redistributions in binary form must reproduce the above copyright
+*     notice, this list of conditions and the following disclaimer in the
+*     documentation and/or other materials provided with the distribution.
+* 3.  Neither the name of MCrux nor the names of its contributors may be 
+*     used to endorse or promote products derived from this software 
+*     without specific prior written permission.
+*
+* @author: Mital Vora.
+**/
 
 #include "StdAfx.h"
 #include "MCruxJSObject.h"
@@ -44,6 +44,7 @@ JSStaticFunction * MCruxJSObject::getStaticFunctions() const
 	static JSStaticFunction mcruxJSDefaultFunctions[]
 	= {
 		{"someFunction", MCruxJSObject::someFunction, 0},
+		{"callJavaScriptFunction", MCruxJSObject::callJavaScriptFunction, 0},
 		{0, 0, 0}
 	};
 	return mcruxJSDefaultFunctions;
@@ -52,4 +53,41 @@ JSStaticFunction * MCruxJSObject::getStaticFunctions() const
 JSObjectCallAsConstructorCallback MCruxJSObject::getConstructor() const
 {
 	return MCruxJSObject::ConstructorCallback;
+}
+
+JSValueRef MCruxJSObject::someFunction(JSContextRef ctx,
+									   JSObjectRef function,
+									   JSObjectRef thisObject,
+									   size_t argumentCount,
+									   const JSValueRef arguments[],
+									   JSValueRef *exception)
+{
+	::MessageBoxA(0, "MCruxJSObject someFunction called", "test", MB_OK);
+	return JSValueMakeNull(ctx);
+}
+
+
+
+JSValueRef MCruxJSObject::callJavaScriptFunction(JSContextRef ctx,
+												 JSObjectRef function,
+												 JSObjectRef thisObject,
+												 size_t argumentCount,
+												 const JSValueRef arguments[],
+												 JSValueRef *exception)
+{
+	::MessageBoxA(0, "MCruxJSObject callJavaScriptFunction called", "test", MB_OK);
+
+	if (argumentCount == 1)
+	{
+		JSStringRef myJSMethodName = JSValueToStringCopy(ctx, arguments[0], 0);
+		JSObjectRef global = JSContextGetGlobalObject(ctx);
+		JSValueRef myJSMethod = JSObjectGetProperty(ctx, global, myJSMethodName, 0);
+		if(JSValueIsObject(ctx, myJSMethod) == false)
+		{
+			return JSValueMakeNull(ctx);
+		}
+		JSObjectRef function = JSValueToObject(ctx, myJSMethod, 0);
+		JSValueRef result = JSObjectCallAsFunction (ctx, function, global, 0, 0, 0);
+	}
+	return JSValueMakeNull(ctx);
 }
--- trunk/MCrux/MCrux/plugin/MCruxJSObject.h
+++ trunk/MCrux/MCrux/plugin/MCruxJSObject.h
@@ -1,21 +1,21 @@
 /**
- * copyright (C) 2008 Mital Vora. All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met:
- *
- * 1.  Redistributions of source code must retain the above copyright
- *     notice, this list of conditions and the following disclaimer.
- * 2.  Redistributions in binary form must reproduce the above copyright
- *     notice, this list of conditions and the following disclaimer in the
- *     documentation and/or other materials provided with the distribution.
- * 3.  Neither the name of MCrux nor the names of its contributors may be 
- *     used to endorse or promote products derived from this software 
- *     without specific prior written permission.
- *
- * @author: Mital Vora.
- **/
+* copyright (C) 2008 Mital Vora. All rights reserved.
+*
+* Redistribution and use in source and binary forms, with or without
+* modification, are permitted provided that the following conditions
+* are met:
+*
+* 1.  Redistributions of source code must retain the above copyright
+*     notice, this list of conditions and the following disclaimer.
+* 2.  Redistributions in binary form must reproduce the above copyright
+*     notice, this list of conditions and the following disclaimer in the
+*     documentation and/or other materials provided with the distribution.
+* 3.  Neither the name of MCrux nor the names of its contributors may be 
+*     used to endorse or promote products derived from this software 
+*     without specific prior written permission.
+*
+* @author: Mital Vora.
+**/
 
 #pragma once
 
@@ -31,11 +31,15 @@ class MCruxJSObject
 		JSObjectRef thisObject,
 		size_t argumentCount,
 		const JSValueRef arguments[],
-		JSValueRef *exception)
-	{
-		::MessageBoxA(0, "MCruxJSObject someFunction called", "test", MB_OK);
-		return 0;
-	}
+		JSValueRef *exception);
+
+	static JSValueRef callJavaScriptFunction(JSContextRef ctx,
+		JSObjectRef function,
+		JSObjectRef thisObject,
+		size_t argumentCount,
+		const JSValueRef arguments[],
+		JSValueRef *exception);
+
 
 	static JSObjectRef ConstructorCallback(JSContextRef ctx,
 		JSObjectRef constructor,
