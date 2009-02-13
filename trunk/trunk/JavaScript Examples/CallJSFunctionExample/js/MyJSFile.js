c2f668d0363e40cd9184acda378dd91c7f371e98
mital.d.vora <mital.d.vora@8fcffb50-ab33-11dd-9a14-c1645b63b0c6>
2009-02-13 01:22:09 +0000

    Adding callJavaScriptFunction() in MCruxJSObject class. now u can call a JavaScriptFunction passing its name to mcrux.callJavaScriptFunction("myfunc").

---
+++ trunk/trunk/JavaScript Examples/CallJSFunctionExample/js/MyJSFile.js
@@ -0,0 +1,10 @@
+function myFunction()
+{
+	alert("CallJSFunction Example!"); 
+	mcrux.callJavaScriptFunction("myFunctionInJavaScript");
+}
+
+function myFunctionInJavaScript()
+{
+	alert("myFunctionInJavaScript Called");
+}
