aa17cb0f0819631db2d1553e5a95915ab27b4c09
mital.d.vora <mital.d.vora@8fcffb50-ab33-11dd-9a14-c1645b63b0c6>
2009-03-22 06:18:25 +0000

    Adding description related to example in HTML files.

--- trunk/trunk/JavaScript Examples/Socket/example1/MyJSFile.js
+++ trunk/trunk/JavaScript Examples/Socket/example1/MyJSFile.js
@@ -5,8 +5,12 @@ function onConnectComplete(event)
 	alert("onConnected Handler Called");
 	alert(event.hostname);
 	alert(event.port);
-	socketObj.send("<?xml version=\"1.0\"?>");
-	socketObj.send("<stream:stream to=\"mital-lappy\" xml:lang=\"en\" xmlns=\"jabber:client\" xmlns:stream=\"http://etherx.jabber.org/streams\" version=\"1.0\">");
+	if(event.connected == true)
+	{
+		alert("we are connected... now opeining a stream:stream");
+		socketObj.send("<?xml version=\"1.0\"?>");
+		socketObj.send("<stream:stream to=\"mital-lappy\" xml:lang=\"en\" xmlns=\"jabber:client\" xmlns:stream=\"http://etherx.jabber.org/streams\" version=\"1.0\">");
+	}
 }
 
 
@@ -20,9 +24,8 @@ function myFunction()
 {
 	alert("socket Example ! creating socketObj object with new operator");
 
-    window.open("http://www.google.com");
-//	socketObj = new mcrux.socket();
-//	socketObj.addEventListener("onConnectComplete", onConnectComplete);
-//	var ret = socketObj.connect("localhost", 5222);
-//	socketObj.addEventListener("onRead", ReadDataHandler);
+	socketObj = new mcrux.socket();
+	socketObj.addEventListener("onConnectComplete", onConnectComplete);
+	socketObj.addEventListener("onRead", ReadDataHandler);
+	var ret = socketObj.connect("localhost", 5222);
 }
