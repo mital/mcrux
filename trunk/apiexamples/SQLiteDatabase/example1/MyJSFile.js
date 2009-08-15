9aee751da90878aeeaf24f2dafd751ee7aae90ba
mital.d.vora <mital.d.vora@8fcffb50-ab33-11dd-9a14-c1645b63b0c6>
2009-08-15 10:45:07 +0000

    Removing Examples From Trunk Repository.

--- trunk/apiexamples/SQLiteDatabase/example1/MyJSFile.js
+++
@@ -1,8 +0,0 @@
-function myFunction()
-{
-	alert("sqlite Example ! creating mydb object with new operator");
-	var mydb = new mcrux.sqlite_database();
-	alert("mydb created successfully now calling someFunction() over it");
-	mydb.someFunction();
-	alert("after SomeFunction call");
-}
--- trunk/apiexamples/data_sharing_two_MCrux_windows/loupe/js/index.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function (){
-	$("#editor").keypress(function (event){
-		if (mcrux.loupeEventHandler)
-			mcrux.loupeEventHandler(String.fromCharCode(event.which));
-	});
-});
